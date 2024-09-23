3d3570cbcf84a52e0feff251ecfa892fdd837d51
gh-johnny <jmfurtadoromero@gmail.com>
2024-09-23 09:54:10 -0300

    add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,13 @@
+import { useLocation } from "react-router-dom"
+
 export function Footer() {
+
+    const { pathname } = useLocation()
+
+    if (pathname === '/login') {
+        return null
+    }
+
     return (
         <>
             Footer
