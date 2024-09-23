5eee2070a7cfed769b85bfc7f303502e54ebbf1e
gh-johnny <jmfurtadoromero@gmail.com>
2024-09-23 10:20:47 -0300

    feat: logout func

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-import { Outlet, useNavigate } from 'react-router-dom'
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
 import { Nav } from './components/Nav'
 import { Footer } from './components/Footer'
-import { useMount } from './hooks/useMount'
+import { useEffect } from 'react'
 
 export default function App() {
 
+    const { pathname } = useLocation()
     const navigate = useNavigate()
 
-    useMount(() => {
+    useEffect(() => {
         const user = JSON.parse(sessionStorage.getItem('user'))
         if (!user || !user.email || user.email.length === 0) navigate('/login')
-    })
+    }, [pathname])
 
     return (
-        <main className='overflow-x-hidden min-w-[320px] min-h-dvh'>
+        <main className='flex flex-col justify-between overflow-x-hidden min-w-[320px] min-h-dvh'>
             <Nav />
             <Outlet />
             <Footer />
