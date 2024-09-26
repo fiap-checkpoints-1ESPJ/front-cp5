0f78c9ef8b5a9493f11eee11b2d3f3efa00d25a6
Kayky Stiliano <145211344+KStiliano@users.noreply.github.com>
2024-09-26 10:08:08 -0300

    Adição da foto

--- src/routes/Sobre.jsx
+++ src/routes/Sobre.jsx
@@ -1,11 +1,12 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import gabrielFoto from "../assets/FotoGabriel.jpg"
 import fotoBradescoro from '../assets/foto_bradescoro.jpg'
+import fotoKayko from '../assets/foto_kayko.jpeg'
 
 export function Sobre() {
   const developers = [
     { name: "João Romero", image: "/placeholder.svg?height=200&width=200&text=João" },
-    { name: "Kayky Stiliano", image: "/placeholder.svg?height=200&width=200&text=Kayky" },
+    { name: "Kayky Stiliano", image: fotoKayko },
     { name: "Pedro Mendes", image: fotoBradescoro },
     { name: "Pedro Bizzo", image: "/placeholder.svg?height=200&width=200&text=Pedro+B" },
     { name: "Gabriel Barros", image: gabrielFoto }
